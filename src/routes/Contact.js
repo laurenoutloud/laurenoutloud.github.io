a017c7380d1605c29c42f2fda6371544d94abbc5
Lauren <lauren.finley1@yahoo.com>
2019-10-11 03:10:48 +0900

    add content to contact page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import linkedInLogo from "../images/linkedinlogo.png";
+import githubLogo from "../images/githublogo.png";
 
-class Contact extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>Contact</h1>
-            </div>
-        );
-    }
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className="contact">
+        <h1>Contact</h1>
+        <p>
+          <strong>email: </strong> lauren.finley01@gmail.com
+        </p>
+        <p>
+            <strong>LinkedIn: </strong>
+            <a href="https://www.linkedin.com/in/laurenfinley01" target="_blank"><img  src={linkedInLogo}/> </a>
+        </p>
+        <p>
+            <strong>Github:</strong>
+            <a href="https://github.com/laurenoutloud" target="_blank">
+                <img src={githubLogo}/>
+            </a>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Contact;
