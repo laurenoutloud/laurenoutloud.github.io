7bb94f8c5b2aa69d651c6623cd6b5b96f485c389
Lauren <lauren.finley1@yahoo.com>
2019-10-11 02:52:02 +0900

    add home page content

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import "../css/Home.css"
+import React from "react";
+import "../css/Home.css";
+import selfPhoto from "../images/graduate-photo.jpg";
 
-class Home extends React.Component{
-    render(){
-        return(
-            <div className="home">
-                <h2>Insightful. Versatile. Ambitious.</h2>
-                <p>"The only thing that stands between a person and what they want in life is the will to try and the faith to believe it is possible."
-					-Rich DeVos</p>
-            </div>
-            
-        )
-    }
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="home-text">
+          <h2>Insightful. Versatile. Ambitious.</h2>
+
+          <p>
+            "The only thing that stands between a person and what they want in
+            life is the will to try and the faith to believe it is possible."
+            -Rich DeVos
+          </p>
+        </div>
+        <img className="homePhoto" src={selfPhoto} alt="Graduation Photo" />
+      </div>
+    );
+  }
 }
 
 export default Home;
