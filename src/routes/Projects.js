4893f63fbdb5d2cc60328df32ac3247a4e047eda
Lauren <lauren.finley1@yahoo.com>
2019-10-11 03:34:22 +0900

    add content to projects page

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
-class Projects extends React.Component{
-    render(){
-        return(
-        <div>
-            <h1>Projects</h1>
-        </div>
-        );
-    }
+class Projects extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Projects</h1>
+        <p>
+          Documentation of current and previous projects will be added shortly,
+          but for now please check out my github by {" "}
+          <a href="https://github.com/laurenoutloud" target="_blank">
+          clicking here!
+          </a>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Projects;
--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import resume from "../documents/resume.pdf";
 class Resume extends React.Component{
     render(){
         return(
             <div>
             <h1>Resume</h1>
+            <p>For a downloadable version, click <a href={resume} target="_blank">here</a>.</p>
             </div>
         );
     }
