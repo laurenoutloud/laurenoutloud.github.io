96f4ccacf77b7287a7857289e8ea23abc7316ce6
Lauren <lauren.finley1@yahoo.com>
2019-10-11 03:51:58 +0900

    add photo to about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import "../css/About.css"
+import naturePhoto from "../images/nature-photo.jpg";
+
 
 class About extends React.Component{
     render(){
         return(
-            <div>
+            <div className="about">
                 <h1>About</h1>
+                <div className="content">
+                <div>
+                <img className="about-image" src={naturePhoto}/>
+                </div>
                 <p>
                 Hi, my name is Lauren! I am a software developer, thrill seeker, and world traveler. I am currently seeking full-time employment opportunities both within the U.S. and abroad as a software engineer.
                 </p>
+                </div>
             </div>
         );
     }
