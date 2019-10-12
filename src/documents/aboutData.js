d75fbed2fa70a52b767a7c786a839bdcbb68cec3
Lauren <lauren.finley1@yahoo.com>
2019-10-12 21:08:47 +0900

    add tabs to about page

---
+++ src/documents/aboutData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import naturePhoto from "../images/nature-photo.jpg";
+
+export const introduction = () => (
+    <div className="introduction">
+        <img className="about-image" src={naturePhoto} />
+        Hi, my name is Lauren! I am a software developer, thrill seeker, and world traveler. I am currently seeking full-time employment opportunities both within the U.S. and abroad as a software engineer.
+    </div>
+);
+
+export const hobbies = () => (
+    <div>Hobbies and Stuff</div>
+);
+
+export const education = () => (
+    <div>Education</div>
+);
+
+export const workExperience = () => (
+    <div>Work</div>
+);
+
+export const affiliateOrganizations = () => (
+    <div>Affiliate Orgs</div>
+);
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/About.css";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import naturePhoto from "../images/nature-photo.jpg";
+import {introduction, hobbies} from "../documents/aboutData";
 
 class About extends React.Component {
   render() {
@@ -11,28 +11,19 @@ class About extends React.Component {
         <h1>About</h1>
         <Tabs>
           <TabList>
-            <Tab>Introduction</Tab>
+            <Tab>Intro</Tab>
             <Tab>Hobbies & Interests</Tab>
             <Tab>Education</Tab>
             <Tab>Work Experience</Tab>
             <Tab>Affiliate Organizations</Tab>
           </TabList>
-          <TabPanel>Introduction</TabPanel>
-          <TabPanel>Hobbies & Interests</TabPanel>
+          <TabPanel>{introduction()}</TabPanel>
+          <TabPanel>{hobbies()}</TabPanel>
           <TabPanel>Education</TabPanel>
           <TabPanel>Work Experience</TabPanel>
           <TabPanel>Affiliate Organizations</TabPanel>
         </Tabs>
         <div className="content">
-          <div>
-            <img className="about-image" src={naturePhoto} />
-          </div>
-          <p id="education">
-            Hi, my name is Lauren! I am a software developer, thrill seeker, and
-            world traveler. I am currently seeking full-time employment
-            opportunities both within the U.S. and abroad as a software
-            engineer.
-          </p>
         </div>
       </div>
     );
