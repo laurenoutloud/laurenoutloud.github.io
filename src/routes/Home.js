28e2cd67db74552e38ef2c32161d2246d41c4857
Lauren <lauren.finley1@yahoo.com>
2019-10-12 23:55:43 +0900

    adjust home photo so it looks nice on all devices

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,16 +6,14 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <div className="home-text">
+        <img className="homePhoto" src={selfPhoto} alt="Graduation Photo" />
           <h2>Insightful. Versatile. Ambitious.</h2>
-
           <p>
             "The only thing that stands between a person and what they want in
             life is the will to try and the faith to believe it is possible."
             -Rich DeVos
           </p>
-        </div>
-        <img className="homePhoto" src={selfPhoto} alt="Graduation Photo" />
+        
       </div>
     );
   }
